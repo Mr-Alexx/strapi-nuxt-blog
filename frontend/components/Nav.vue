732347ba82cd3091129e0c6005d382ebf00fb4a2
Mr.Alexx <719876205@qq.com>
2025-04-29 00:47:11 +0800

    ✨ feat(frontend): 增加组件
    1.增加多语言时间格式化显示组件
    2.增加SvgIcon组件
    3.修改Nav组件样式

--- frontend/components/Nav.vue
+++ frontend/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- 顶部导航栏 -->
   <header class="sticky top-0 z-50 bg-white">
-    <div class="mx-auto px-4">
-      <div class="flex items-center justify-between h-16">
+    <div class="mx-auto md:px-6">
+      <div class="flex items-center justify-between h-60px">
         <!-- 移动端显示的标题/LOGO -->
         <div class="flex md:hidden items-center">
           <h1 class="text-xl font-bold">Alex</h1>
@@ -22,23 +22,10 @@
         </nav>
         
         <!-- 语言切换 -->
-        <div class="relative group">
-          <span class="flex items-center text-gray-700 cursor-pointer hover:text-blue-600 transition-colors">
-            <span>{{ currentLocaleName }}</span>
-            <span class="i-carbon-chevron-down ml-1"></span>
+        <div class="relative group text-gray-500 text-14px">
+          <span class="flex items-center cursor-pointer hover:text-blue-600 transition-colors">
+            <span @click="switchLanguage(anotherLocale.code)">{{ anotherLocale.name }}</span>
           </span>
-          <div class="absolute right-0 mt-2 w-36 bg-white rounded-md shadow-lg overflow-hidden z-50 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200">
-            <div class="py-1">
-              <span 
-                v-for="locale in allLocales" 
-                :key="locale.code" 
-                @click="switchLanguage(locale.code)"
-                class="block w-full text-left px-4 py-2 text-sm cursor-pointer text-gray-700 hover:bg-gray-100 user-select-none"
-              >
-                {{ locale.name }}
-              </span>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -54,15 +41,8 @@ const route = useRoute()
 const { t, locale, locales, setLocale } = useI18n()
 const localePath = useLocalePath()
 
-// 处理所有可用的语言
-const allLocales = computed(() => {
-  return locales.value
-})
-
-// 当前语言名称
-const currentLocaleName = computed(() => {
-  const current = allLocales.value.find(l => l.code === locale.value)
-  return current ? current.name : ''
+const anotherLocale = computed(() => {
+  return locales.value.find(l => l.code !== locale.value)
 })
 
 // 切换语言
